d12dbf1d225a1ea2f2f103b5d5c0ea1054337743
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-08 13:30:16 -0700

    Added context, viewCards component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+const AppContext = React.createContext('light')
+
 export default class App extends React.Component {
   render() {
     return <h1 className="text-center">Flash Card App</h1>
---
+++ src/components/view-cards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class ViewCards extends React.Component {
+  render() {
+    return (
+      <h1>My Cards</h1>
+    )
+  }
+}
