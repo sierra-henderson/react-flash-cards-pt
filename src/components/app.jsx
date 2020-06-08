8c8bca83f54174ce7d1c6419a0cad7c9d77912ab
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-08 15:18:33 -0700

    Created getView method, altered App.render to include context and different views

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,19 +3,40 @@ import ViewCards from './view-cards'
 import ReviewCards from './review-cards'
 import CreateCard from './create-card'
 
-const AppContext = React.createContext('light')
+const AppContext = React.createContext()
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = { view: 'view-cards' }
+    this.setView = this.setView.bind(this)
+    this.getView = this.getView.bind(this)
   }
 
   setView(view) {
     this.setState({ view })
   }
 
+  getView() {
+    switch(this.state.view) {
+      case 'create-card':
+        return <CreateCard />;
+      case 'review-cards':
+        return <Review />;
+      case 'view-cards':
+        return <ViewCards />;
+      default:
+        return null;
+    }
+  }
+
   render() {
-    return <h1 className="text-center">Flash Card App</h1>
+    return (
+      <AppContext.Provider value={this.state.view}>
+        <div>
+          {this.getView()}
+        </div>
+      </AppContext.Provider>
+    )
   }
 }
