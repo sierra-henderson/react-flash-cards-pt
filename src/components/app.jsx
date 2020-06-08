e818d63d95ae6b13859bc1cd3d10aece4511e434
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-08 14:47:19 -0700

    Added view property to state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,6 +6,11 @@ import CreateCard from './create-card'
 const AppContext = React.createContext('light')
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { view: 'view-cards' }
+  }
+
   render() {
     return <h1 className="text-center">Flash Card App</h1>
   }
