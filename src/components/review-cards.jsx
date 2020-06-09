515faeb63e8168a7c7ae04b44d18cbb83af06a24
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-09 11:41:37 -0700

    Added side property in state

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { AppContext } from './app'
 
 export default class ReviewCards extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { side: 'front' }
+  }
+
   componentDidMount() {
     this.props.setActiveCard(0)
   }
